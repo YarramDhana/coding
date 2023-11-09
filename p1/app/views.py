5f97aa261e69b6b6bab8f1781e6427e3063a774b
YarramDhana <yarramdhana9156@gmail.com>
2023-11-09 09:28:47 +0530

    first commit

---
+++ p1/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+    
+
+# Create your views here
+def chinnu (request):
+    return HttpResponse ('hii chinnu how are you')
+
+    
