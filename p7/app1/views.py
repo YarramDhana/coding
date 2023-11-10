16f85802b0f12e7a584832518eb2733228a94dc7
YarramDhana <yarramdhana9156@gmail.com>
2023-11-10 12:02:49 +0530

    first commit

---
+++ p7/app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def  developer (request):
+    return HttpResponse ("hii cherry what are you doing")
